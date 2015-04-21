abf7a143ab0d17e2857510ac2dff1c2e4af7929c
Stephen Begley <stephenbegley3@gmail.com>
2015-04-21 21:05:06 +0100

    azure

---
+++ RollCallV8/RollCallV8/Models/Enrollment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RollCallV8.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+
+        public Grade? Grade { get; set; }
+
+
+        //navigation properties
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ RollCallV8/RollCallV8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RollCallV8.Startup))]
+namespace RollCallV8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
